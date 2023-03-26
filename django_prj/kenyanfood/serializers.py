66ac5d225a726cf36cfb548256b0708c25760320
MuskanMainuddin-code <404patelmuskan@gmail.com>
2023-03-26 23:14:00 +0530

    get api

---
+++ django_prj/kenyanfood/serializers.py
@@ -0,0 +1,8 @@
+# import serializer from rest_framework
+# from rest_framework import serializers
+# from kenyanfood import models
+
+# create a serializer
+# class FoodSerializer(serializers.Serializer):
+    # initialize fields
+    # data = serializers.serialize('json', models.Food.objects.all())
--- django_prj/kenyanfood/views.py
+++ django_prj/kenyanfood/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from kenyanfood import models
+from kenyanfood import serializers
 
 # Create your views here.
 @api_view(['GET'])
 def getFood(request):
-	return Response()
+	food_obj = models.Food.objects.all().values()
+	print("----------------food_obj")
+	print(food_obj)
+	# serializer_class = serializers.FoodSerializer
+	return Response(food_obj)
